2b83448e6335b83aeca323a9483dcd71c97b2ff2
EricGit <ericrothstack@gmail.com>
2021-01-15 09:19:37 +0500

    text edited

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
     <div className="App">
       <Container maxWidth="sm">
         <Typography variant="h1" component="h2" gutterBottom>
-          TODO LIST for learning
+          TODO LIST for Redux
         </Typography>
 
         <form className={classes.root} noValidate autoComplete="off">
